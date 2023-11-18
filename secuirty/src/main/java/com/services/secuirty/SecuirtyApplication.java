f2472d4096559ca8a9566b729d27a0544e222275
prakashponali <p.ponali@gamil.com>
2023-11-18 11:13:30 +0530

    pusing remaining modules

---
+++ secuirty/src/main/java/com/services/secuirty/SecuirtyApplication.java
@@ -0,0 +1,13 @@
+package com.services.secuirty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuirtyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuirtyApplication.class, args);
+    }
+
+}
---
+++ secuirty/src/test/java/com/services/secuirty/SecuirtyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services.secuirty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuirtyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
