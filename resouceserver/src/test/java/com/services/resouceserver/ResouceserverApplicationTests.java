5830191cb624e3a75865c757bcf4ddc4780290ba
prakashponali <p.ponali@gamil.com>
2023-11-18 11:07:40 +0530

    resource server

---
+++ resouceserver/src/test/java/com/services/resouceserver/ResouceserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services.resouceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResouceserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
