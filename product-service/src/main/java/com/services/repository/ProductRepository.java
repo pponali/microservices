825396233aae8826a6c1c9ab2b8b3c10d43e3be8
prakashponali <p.ponali@gamil.com>
2024-01-08 21:33:55 +0530

    commerce application.

--- product-service/src/main/java/com/services/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.services.repository;
-
-import com.services.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
