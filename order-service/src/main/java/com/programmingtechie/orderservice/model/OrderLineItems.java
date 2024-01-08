9fb2e058c483dfa39935c63e58d50c99d8ffa9fd
prakashponali <p.ponali@gamil.com>
2024-01-08 18:47:12 +0530

    test framework

--- order-service/src/main/java/com/programmingtechie/orderservice/model/OrderLineItems.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/model/OrderLineItems.java
@@ -1,11 +1,16 @@
 package com.programmingtechie.orderservice.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 
-import javax.persistence.*;
+
 import java.math.BigDecimal;
 
 @Entity
