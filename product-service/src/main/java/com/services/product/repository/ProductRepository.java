ddb629c5d03b5ca0f40103b92997c3bae1b28266
prakashponali <p.ponali@gamil.com>
2024-01-09 04:15:44 +0530

    fix for build errors.

--- product-service/src/main/java/com/services/product/repository/ProductRepository.java
+++ product-service/src/main/java/com/services/product/repository/ProductRepository.java
@@ -3,5 +3,7 @@ package com.services.product.repository;
 import com.services.product.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+@org.springframework.stereotype.Repository("productRepository")
+@org.springframework.transaction.annotation.Transactional(readOnly = true)
 public interface ProductRepository extends MongoRepository<Product, String> {
 }
